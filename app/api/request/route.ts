2d10a43035caef1de434926e5ee0602a4e033415
Tokestone420 <129816837+Tokestone420@users.noreply.github.com>
2023-11-27 18:26:31 -0500

    Update route.ts

--- app/api/request/route.ts
+++ app/api/request/route.ts
@@ -6,14 +6,14 @@ export const dynamic = 'force-dynamic';
 const ZBD_BASE_URL = 'https://api.zebedee.io';
 const ZBD_API_KEY = 'b7YW3s2JzZKGcXjIf5Dqof8wjKT2RuWr8';
 
-export async function GET() {
+export async function GET() {100000 satoshi
   const res = await fetch(`${ZBD_BASE_URL}/v0/charges`, {
-    method: 'POST',
+    method: 'POST',recieve lightening payment
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json',Run
       'apikey': `${ZBD_API_KEY}`,kcrane2487@zbd.gg
     },
-    body: JSON.stringify({
+    body: JSON.stringify({kcrane2487@zbd.gg
       amount: '1000000', // 1000 satoshis (1,000,000 msats) -- ~$0.48
       description: 'Money at internet speed', // What is this payment request for?
     }),
